1ba95351ef4958577cf2b28830a4608cb7d2e8f3
Davide Fiocco <davide.fiocco@frontiersin.net>
2020-06-28 09:50:29 +0200

    Trying to fix crashes on large images

--- fastapi/segmentation.py
+++ fastapi/segmentation.py
@@ -14,16 +14,19 @@ def get_segmentator():
     return model
 
 
-def get_segments(model, binary_image):
+def get_segments(model, binary_image, max_size=512):
 
     input_image = Image.open(io.BytesIO(binary_image)).convert("RGB")
+    width, height = input_image.size
+    resize_factor = min(max_size/width, max_size/height)
+    resized_image = input_image.resize((int(input_image.width*resize_factor), int(input_image.height*resize_factor)))
 
     preprocess = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
-    input_tensor = preprocess(input_image)
+    input_tensor = preprocess(resized_image)
     input_batch = input_tensor.unsqueeze(0)  # create a mini-batch as expected by the model
 
     with torch.no_grad():
