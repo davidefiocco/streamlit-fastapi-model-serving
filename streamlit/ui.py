a579b34eeab7aa7c531a56d8a6371249ca478d56
Davide Fiocco <davide.fiocco@outlook.com>
2020-09-20 13:02:53 +0200

    make ui code easier to read

--- streamlit/ui.py
+++ streamlit/ui.py
@@ -4,18 +4,9 @@ import requests
 from PIL import Image
 import io
 
-st.title('DeepLabV3 image segmentation')
-
-# fastapi endpoint
+# interact with FastAPI endpoint
 url = 'http://fastapi:8000'
 endpoint = '/segmentation'
-
-st.write('''Obtain semantic segmentation maps of the image in input via DeepLabV3 implemented in PyTorch.
-         This streamlit example uses a FastAPI service as backend.
-         Visit this URL at `:8000/docs` for FastAPI documentation.''')  # description and instructions
-
-image = st.file_uploader('insert image')  # image upload widget
-
 
 def process(image, server_url: str):
 
@@ -30,12 +21,21 @@ def process(image, server_url: str):
 
     return r
 
+# construct UI layout
+
+st.title('DeepLabV3 image segmentation')
+
+st.write('''Obtain semantic segmentation maps of the image in input via DeepLabV3 implemented in PyTorch.
+         This streamlit example uses a FastAPI service as backend.
+         Visit this URL at `:8000/docs` for FastAPI documentation.''')  # description and instructions
+
+input_image = st.file_uploader('insert image')  # image upload widget
 
 if st.button('Get segmentation map'):
 
-    if image is None:
+    if input_image is None:
         st.write("Insert an image!")  # handle case with no image
     else:
-        segments = process(image, url+endpoint)
+        segments = process(input_image, url+endpoint)
         segmented_image = Image.open(io.BytesIO(segments.content)).convert('RGB')
-        st.image([image, segmented_image], width=300)  # output dyptich
+        st.image([input_image, segmented_image], width=300)  # output dyptich
